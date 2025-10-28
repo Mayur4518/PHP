9b185e218e7736fcb30fcfb8fce4662ae26d90b9
Mayur Pawar <mayurpawar4518@gmail.com>
2025-10-28 18:07:57 +0530

    Add files via upload

---
+++ PHP Practical Slips/fun.php
@@ -0,0 +1,22 @@
+<?php
+    function add($a,$b)
+    {
+     $c=$a+$b;
+     return $c;
+    }
+    function sub($a,$b)
+    {
+     $c=$a-$b;
+     return $c;
+     }
+    function multi($a,$b)
+    {
+     $c=$a*$b;
+     return $c;
+    }
+    function div($a,$b)
+    {
+     $c=$a/$b;
+     return $c;
+     }
+?>
